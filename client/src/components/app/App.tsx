9bb36f6e80258717adfed2ceb05e59d7914edfe7
corradowaver <corradowave1@gmail.com>
2019-10-22 22:55:46 +0300

    add scss

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from '../../resourses/logo.svg';
-import './App.css';
+import Navbar from '../navbar/Navbar';
+import Mainview from '../Mainview/Mainview'
+import './App.scss';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Mainview></Mainview>
     </div>
   );
 }
