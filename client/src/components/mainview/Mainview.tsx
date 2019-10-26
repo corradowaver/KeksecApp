fc3feae3e880449290f9780845a63d28cf164f91
corradowaver <corradowave1@gmail.com>
2019-10-27 00:05:13 +0300

    some madens were maden

--- client/src/components/mainview/Mainview.tsx
+++ client/src/components/mainview/Mainview.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 import "./Mainview.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./forms/FormJS"
+import Button from 'react-bootstrap/Button';
 import FormJS from "./forms/FormJS";
 import FormTS from "./forms/FormTS";
+// import Compiler from "./compileform/Compiler"
 
-const Mainview: React.FC = () => {
+const Mainview: React.FC = (props: any) => {
+  const [text] = React.useState<string>("")
+
+  function runCompilation(e: React.MouseEvent): void {
+    console.log(text)
+  }
+
   return (
     <div className="Mainview">
       <div className="InputForms">
-        <FormJS/>
+        <FormJS propToPass={text}/>
         <FormTS/>
       </div>
+      <Button className="CompileButton" variant="outline-danger" onClick={runCompilation}>
+        Compile
+      </Button>
+      {/* <div className="compilerForm">
+        <Compiler/>
+      </div> */}
     </div>
   );
 }
--- client/src/components/mainview/forms/FormJS.tsx
+++ client/src/components/mainview/forms/FormJS.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./FormJS.css"
 
-const FormJS: React.FC = () => {
+interface PassedProps extends React.Props<string> {
+  propToPass: string
+}
+
+const FormJS: React.FC<PassedProps> = (props: any) => {
+ 
+  function handleInput(): void {
+  }
+
   return (
     <div className="FormJS">
-        <textarea className="InputForm">
+        <textarea id="mytextArea" className="InputForm" onInput={handleInput}>
+          {props.propToPass}
         </textarea>
     </div>
   );
